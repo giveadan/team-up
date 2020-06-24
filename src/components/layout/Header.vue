51c9e6800753e7a736e3e85af3c27410add87cd1
Dan Buzaid <danbuzaid@gmail.com>
2020-06-24 13:47:25 -0500

    router

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <h1>This is a header</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    header {
+        padding: 20px;
+    }
+
+</style>
